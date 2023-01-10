115a9dfc0b5bacbb5133385d6513b1ebe25504a3
ngiad <follownghia001@gmail.com>
2023-01-10 13:06:15 +0700

    okee

--- src/Controllers/ProductController.js
+++ src/Controllers/ProductController.js
@@ -1,5 +1,29 @@
 import { ProductSendo } from "../Models/ProductModel.js"
 
+
+const SetQuery = (obj) => {
+    let output = {...obj}
+
+    if(Number(obj.gtprice) < 0){
+        delete output.levelPrice 
+
+        output.sale_price_min = {$lt: obj.ltprice}
+    }
+
+    if(Number(obj.gtprice) > 0 && !obj.ltprice){
+        delete output.levelPrice 
+
+        output.sale_price_min = { $gt: obj.gtprice}
+    }
+
+    if(Number( obj.gtprice) > 0  && Number(obj.ltprice)  > 0 && Number(obj.ltprice)  > Number( obj.gtprice)){
+        output.sale_price_min = { $gt: Number( obj.gtprice),$lt:  Number(obj.ltprice)}
+    }
+    
+    return output
+    
+}
+
 export const GetProduct = async(req,res,next) => {
     const { limit,count } = req.query
 
@@ -13,7 +37,7 @@ export const GetProduct = async(req,res,next) => {
             throw new Error("limit and count is null!")
         }
 
-        const Products = await ProductSendo.find(query).skip(count).limit(limit*30)
+        const Products = await ProductSendo.find(SetQuery(query)).skip(count).limit(limit*30)
         res.json({Products,Length : Products.length})
 
     } catch (error) {
